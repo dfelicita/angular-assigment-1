c898a20cb20c386ab8adecf6d47f13feb5baee42
Daniel Felicita <daniel.felicita@gmail.com>
2017-09-13 15:05:56 -0500

    no message

--- app.js
+++ app.js
@@ -14,7 +14,13 @@
 				$scope.isRed = false;
 				$scope.Response = "OK";
 				var array = $scope.menu.split(',');
-    			console.log('array', array);
+				var contar = 0;
+				array.forEach(function(res, i){
+					var tam = res.trim().length;
+					if(tam > 0)
+						contar++;
+				});
+    			console.log('contar: ', contar);
 			}else{
 				$scope.isRed = true;
 				$scope.Response = "Please enter data first";
