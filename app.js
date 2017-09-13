68c772abc4aa7c016e71375b561b0b340336a571
Daniel Felicita <daniel.felicita@gmail.com>
2017-09-13 11:03:25 -0500

    no message

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 (function () {
 'use strict';
 
-angular.module('Lunch-app', [])
-.controller('LunchController', LunchController);
+	angular.module('LunchApp', [])
+	.controller('LunchController', LunchController);
 
-LunchController.$inject = ['$scope'];
-$scope.menu = "";
-function Check() {
-  console.log('Click');
-}
+	LunchController.$inject = ['$scope'];
+	$scope.menu = "";
+	function LunchController($scope) {
+	  	
+		$scope.click = function(){
+			console.log('Click!!!');
+		}
+
+	}
 
 })();
